0c72751c5429201cb642dbfeaf5748255ed2ff61
jake <jakewdr@proton.me>
2024-11-06 11:30:19 +0000

    Updated dependency and better documentation

--- src/__main__.py
+++ src/__main__.py
@@ -4,9 +4,9 @@ import file2
 
 def main():
     print("Hello World")
-    response = requests.get("https://httpbin.org/ip")
+    response = requests.get("https://httpbin.org/ip")  # Using requests from the bundle!
     print("Your IP is {0}".format(response.json()["origin"]))
-    file2.test()
+    file2.test()  # Example of using a local file
 
 
 if __name__ == "__main__":
