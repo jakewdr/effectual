259c4d2287760ac4a8e7ff2b0c8e4b669c6535ac
jake <jakewdr@proton.me>
2024-11-12 14:39:00 +0000

    Better example code

--- src/__main__.py
+++ src/__main__.py
@@ -1,12 +1,25 @@
 import requests
 import file2
+import os
 
 
-def main():
+def main() -> None:
     print("Hello World")
+    bundleCheck()
     response = requests.get("https://httpbin.org/ip")  # Using requests from the bundle!
     print("Your IP is {0}".format(response.json()["origin"]))
     file2.test()  # Example of using a local file
+
+
+def bundleCheck() -> None:
+    scriptPath: str = str(
+        os.path.realpath(__file__).replace(os.sep, "/")
+    )  # Gets the path of the current running python script and makes sure forward-slashes are used
+
+    if "bundle.py" in scriptPath:
+        print("Running in bundle!")
+    else:
+        print("Running in developer mode!")
 
 
 if __name__ == "__main__":
