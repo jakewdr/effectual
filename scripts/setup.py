621f6122ed7b61fb83d5bc736e4734b88078c5bc
jake <jakewdr@proton.me>
2024-11-12 18:00:09 +0000

    Hopefully made the project more platform agnostic

--- scripts/setup.py
+++ scripts/setup.py
@@ -6,16 +6,20 @@ def main() -> None:
     with open("./Pipfile", "rb") as file:
         packages: dict = dict((tomllib.load(file)).get("packages"))
 
+    arguments: list[str] = ["--no-compile", "--quiet", "--upgrade", "--no-binary=none"]
+
+    argumentString: str = " ".join(arguments)
+
     for key in packages:
         print(f"Installing {key}")
         pathToInstallTo: str = "./.effectual_cache/cachedPackages"
         if packages.get(key) == "*":
             os.system(
-                f"pipenv run pip install {key} --no-compile --quiet --exists-action=i --target {pathToInstallTo}"
+                f"pipenv run pip install {key} {argumentString} --target {pathToInstallTo}"
             )
         else:
             os.system(
-                f"pipenv run pip install {key}=={packages.get(key)} --no-compile --quiet --exists-action=i --target{pathToInstallTo}"
+                f"pipenv run pip install {key}=={packages.get(key)} {argumentString} --target {pathToInstallTo}"
             )
 
     print("Finished installing current dependencies")
