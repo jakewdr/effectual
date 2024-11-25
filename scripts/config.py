650965fe3af73003244f89966a11df0bd4f1e396
jake <jakewdr@proton.me>
2024-11-25 13:39:01 +0000

    Added docstring for loadconfig

--- scripts/config.py
+++ scripts/config.py
@@ -2,6 +2,18 @@ import rtoml
 
 
 def loadConfig(configPath: str) -> dict:
+    """Loads effectual config from a file
+
+    Args:
+        configPath (str): Path to the config file
+
+    Raises:
+        RuntimeError: Invalid TOML format
+        RuntimeError: No configuration file found
+
+    Returns:
+        dict: _description_
+    """
     try:
         with open(configPath, "r", encoding="utf-8") as file:
             tomlFile: dict = dict(rtoml.load(file))
