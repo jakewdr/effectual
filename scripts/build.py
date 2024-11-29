a97575f6c0672141547b227a809424aacb878c00
jake <jakewdr@proton.me>
2024-11-29 10:10:36 +0000

    Build time improvement
    - Using async

--- scripts/build.py
+++ scripts/build.py
@@ -79,10 +79,10 @@ def dependencies(minify: bool) -> None:
     print(f"{tagColor('optimizing')} || {','.join(packages)}")
 
     with Pool(os.cpu_count()) as pool:
-        pool.map(optimizeDependencies, Path(pathToInstallTo).rglob("*"))
+        pool.map_async(optimizeDependencies, Path(pathToInstallTo).rglob("*"))
 
 
-def optimizeDependencies(file: Path) -> None:
+async def optimizeDependencies(file: Path) -> None:
     if (
         file.suffix in (".pyc", ".pyd", ".exe", ".typed")
         or "__pycache__" in str(file)
